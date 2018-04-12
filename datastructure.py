40713095c66a12553e1acb34af5f55668207c473
Liuyang-cn <liuyang_maple@hotmail.com>
2018-04-12 20:52:35 +0800

    列表推导式

--- datastructure.py
+++ datastructure.py
@@ -30,5 +30,18 @@ print(queue)
 '''
 # 列表推导式
 vec = [2, 4, 6]
-#print([3 * x for x in vec])  # 用方括号表示推导出的是一个元组
-print([x, x * 2] for x in vec)
+print([3 * x for x in vec])  # 用方括号表示推导出的是一个元组
+print([[x, x * 2] for x in vec])
+#如果少加外层方括号 报错
+#<generator object <genexpr> at 0x0000014DB06C3CA8>
+#'''
+#对序列里每一个元素逐步调用某方法 weapon.strip()
+freshfruit = ['banana', ' loganberry', 'passion fruit']
+print([weapon.strip() for weapon in freshfruit])
+
+#用if做过滤器
+print([3 * x for x in vec if x > 3])
+print([x * y for x in vec if x < 4 for y in vec if y > 4])
+print([vec[i] * vec[i - 1] for i in range(len(vec))])  #vec[-1]倒数第一个元素
+#float int str
+print([float(round(355 / 113, i)) for i in range(1, 6)])
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # learn python notes
 
 * 这两个练习跟的廖雪峰
-1.  learnfunctions-liao.py    函数参数练习
-2.  functions.py         函数参数练习二
+1. learnfunctions-liao.py    函数参数练习
+2. functions.py         函数参数练习二
 
 * 菜鸟教程
 1. function.py                函数练习
