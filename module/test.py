ca7c684f456448d68a362201b56cb69e57f1131d
Liuyang-cn <liuyang_maple@hotmail.com>
2018-04-13 18:34:41 +0800

    add fibo.py

--- module/test.py
+++ module/test.py
@@ -1,6 +1,18 @@
 #!usr/bin/python3
 #
-
+#导入模块 import
 import support
+#import fibo
+
+#导入模块的指定部分 from import
+from fibo import fib, fib2
+#导入模块的全部内容
+#from modname import *
 
 support.print_func("Liuyang")
+'''
+#import fibo test
+fib = fibo.fib
+print(fib(100))
+'''
+print(fib(100))
