1a75f74cfee4bdbf9c981fd28a3a15f76ef51144
Liuyang-cn <liuyang_maple@hotmail.com>
2018-04-19 17:12:15 +0800

    I don't what has been changed

--- function.py
+++ function.py
@@ -117,21 +117,29 @@ def outer():
 #### global 和 nonlocal 关键字
 
 num = 1
+
+
 def fun1():
     # 调用 全局变量
-    global num      #需要使用global关键字声明
+    global num  #需要使用global关键字声明
     print(num)
     num = 123
     print(num)
+
+
 fun1()
+
 
 def outer():
     num = 10
+
     def inner():
-        nonlocal num    # nonlocal 关键字声明
+        nonlocal num  # nonlocal 关键字声明
         num = 100
         print(num)
+
     inner()
     print(num)
-outer()
 
+
+outer()
