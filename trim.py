e5a11585178cbfd1c164a56d28d5a8ce3e22c367
Liuyang-cn <liuyang_maple@hotmail.com>
2018-11-24 20:31:27 +0800

    trim

--- trim.py
+++ trim.py
@@ -1,16 +1,22 @@
 #!/user/env/bin python3
 #! -*- codint:utf-8 -*-
 
+
 def trim(s):
     print(len(s))
-    while s[0:1] ==' ':
-        s=s[1:]
-        print("minus left once",len(s))
-    while s[-2:-1] ==' ':
-        s=s[:-1]
-        print("minus right once",len(s))
+    while s[0:1] == ' ':
+        s = s[1:]
+        print("minus left once", len(s))
+    while s[-1:] == ' ':
+        print("'%s'" % s[-2:-1])
+        s = s[:-1]
+        print("'%s'" % s)
+        print("'%s'" % s[:-1])
+        print("'%s'" % s[-2:-1])
+        print("minus right once", len(s))
     return s
 
-s='  hello       '
-s=trim(s)
-print(s,len(s))
+
+s = '  hello         '
+s = trim(s)
+print("the string s is'%s',its lenth is" % s, len(s))
