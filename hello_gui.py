24072826432b8d145212cf7e735ef2847b66fe01
Liuyang-cn <liuyang_maple@hotmail.com>
2018-11-28 13:22:59 +0800

    formate

--- hello_gui.py
+++ hello_gui.py
@@ -2,18 +2,21 @@
 #!-*- coding:utf-8 -*-
 
 import tkinter
+
+
 class Application(Frame):
-    def __init__(self,master=None):
-        Frame.__init__(self,master)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
-    def createWidgets():
-        self.helloLabel = Lable(self,text='Hello,world!')
-        self.hellobale.pack()
-        self.quitButton=Button(self,text='Quit',command=self.quit)
-        self.quitButton.pack()
-        
 
-app=Application()
+    def createWidgets():
+        self.helloLabel = Lable(self, text='Hello,world!')
+        self.hellobale.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
 app.master.title('Hello World')
 app.manloop()
