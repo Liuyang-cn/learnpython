85e88fc30cb4e99fbdfcd3a9f2c7ec7a29b5edfa
Liuyang-cn <liuyang_maple@hotmail.com>
2018-12-12 22:28:08 +0800

    async

--- awesome-python3-webapp/www/orm.py
+++ awesome-python3-webapp/www/orm.py
@@ -19,58 +19,43 @@ async def create_pool(loop, **kw):
         port=kw.get('port', 'localhost'),
         user=kw['user'],
         password=kw['password'],
-<<<<<<< HEAD
-        db=kw['db'])
-=======
-        db=kw['db']),
+        db=kw['db'],
         charset=kw.get('charset','utf8'),
         autocommit=kw.get('autocommit',True),
         maxsize=kw.get('maxsize',10),
         minisize=kw.get('minisize',1),
         loop=loop
         )
->>>>>>> 478d98b2714cb106a43e27eac687d3a2fd705add
 
 #select
-#@asyncio.coroutine
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
-    with (await __pool) as conn:
-        cur = await conn.cursor(aiomysql.DictCursor)
-        await cur.execute(sql.replace('?', '%s'), args or ())
-        if size:
-            rs = await cur.fetchmany(size)
-        else:
-            rs = await cur.fetchall()
-        await cur.close()
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?','%s'),arg or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
         logging.info('rows returned:%s' % len(rs))
         return rs
 
 # insert,update,delete 语句定义一个通用的execute()
-<<<<<<< HEAD
-async def execute(sql,args):
-=======
 async def execute(sql,args,autocommit=(size)):
->>>>>>> 478d98b2714cb106a43e27eac687d3a2fd705add
     log(sql)
-    with (await __pool) as conn:
+    async with (await __pool) as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = await conn.cursor()
-            await cur.execute(sql.replace('?','%s'))
-            affected=cur.rowcount
-<<<<<<< HEAD
-            await from cur.close()
-        exicept BaseException as e:
-=======
-            await cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?','%s'),args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
->>>>>>> 478d98b2714cb106a43e27eac687d3a2fd705add
+            if not autocommit:
+                await conn.rollback()
             raise
         return affected
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 478d98b2714cb106a43e27eac687d3a2fd705add
