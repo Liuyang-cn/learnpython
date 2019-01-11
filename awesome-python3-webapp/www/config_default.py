28ae0b396ba329b629a45833599310e1bae3cd8b
Liuyang-cn <liuyang_maple@hotmail.com>
2019-01-11 22:24:16 +0800

    comfig

---
+++ awesome-python3-webapp/www/config_default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3066,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
--- awesome-python3-webapp/www/config_override.py
+++ awesome-python3-webapp/www/config_override.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #config_overide_py
 
 configs = {'db': {'host': '192.168.1.100'}}
